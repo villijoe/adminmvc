2030af10c20b4481e696193df365defea50235f9
villijoe <joe_joe_barabanshi@mail.ru>
2016-04-23 11:25:38 +0300

    delete extra methods from controller

--- controllers/BooksController.php
+++ controllers/BooksController.php
@@ -5,49 +5,4 @@ include_once ROOT . '/controllers/ParentController.php';
 
 class BooksController extends ParentController
 {
-    /**
-     * @desc func index page books
-     * @return bool
-     */
-    /*public function actionIndex()
-    {
-        $booksList = array();
-        $model_name = str_replace('Controller', '', get_class());
-        $booksList = $model_name::getBooksList();
-
-        require_once(ROOT . '/views/book/index.php');
-
-        return true;
-    }
-
-    public function actionView($id)
-    {
-        if ($id) {
-            $booksItem = Books::getBooksItemById($id);
-
-            require_once(ROOT . '/views/book/book.php');
-
-
-        }
-
-        return true;
-    }*/
-
-    /*public function actionFinished()
-    {
-        $booksFinishedList = Books::getFinishedBooks();
-
-        require_once(ROOT . '/views/book/finished.php');
-
-        return true;
-    }*/
-
-/*    public function actionProcess()
-    {
-        $booksProcessList = Books::getProcessBooks();
-
-        require_once(ROOT . '/views/book/process.php');
-
-        return true;
-    }*/
 }
--- controllers/GamesController.php
+++ controllers/GamesController.php
@@ -5,44 +5,4 @@ include_once ROOT . '/controllers/ParentController.php';
 
 class GamesController extends ParentController
 {
-    /*public function actionIndex()
-    {
-        $gamesList = array();
-        $gamesList = Games::getGamesList();
-
-        require_once(ROOT . '/views/game/index.php');
-
-        return true;
-    }
-
-    public function actionView($id)
-    {
-        if ($id) {
-            $gamesItem = Games::getGamesItemById($id);
-
-            require_once(ROOT . '/views/game/game.php');
-
-
-        }
-
-        return true;
-    }
-
-    public function actionFinished()
-    {
-        $gamesFinishedList = Games::getFinishedGames();
-
-        require_once(ROOT . '/views/game/finished.php');
-
-        return true;
-    }
-
-    public function actionProcess()
-    {
-        $gamesProcessList = Games::getProcessGames();
-
-        require_once(ROOT . '/views/game/process.php');
-
-        return true;
-    }*/
 }
